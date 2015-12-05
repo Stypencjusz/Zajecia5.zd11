4dbe3aa69f754e72f8a2c2f888996813b3c9c0c9
Stypencjusz <b.stypczynski@outlook.com>
2015-12-05 17:26:29 +0100

    ZD11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        double Cyfra;
+        double Cyfra,lp=0;
         Scanner odczyt = new Scanner(System.in);
         System.out.println("Podaj liczbe calkowita");
         Cyfra = odczyt.nextInt();
         for (int i = 1; i <= Cyfra; i++) {
-            if ((Cyfra % i) == 0)
+            if ((Cyfra % i) == 0) {
                 System.out.println(" " + i);
+                lp++;
+            }
         }
+        if (lp==2)
+            System.out.println(" Liczba " + Cyfra +" jest pierwsza");
     }
 }
